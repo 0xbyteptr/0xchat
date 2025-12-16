4e9262751d80283f3e30ac2a5f126b1c23727219
Byteptr <me@byteptr.xyz>
2025-12-16 12:43:06 +0100

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -9,23 +9,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    if (process.env.NODE_ENV === "development") {
-      return {
-        beforeFiles: [
-          {
-            source: "/api/:path*",
-            destination: "http://localhost:3001/api/:path*",
-          },
-          {
-            source: "/cdn/:path*",
-            destination: "http://localhost:3003/cdn/:path*",
-          },
-        ],
-      };
-    }
-    return { beforeFiles: [] };
-  },
 };
 
 export default nextConfig;
